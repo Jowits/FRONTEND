638fcc09dd17aafd1048c0e3fe82dd20b2b5691e
Jowita <jowita.lapies@gmail.com>
2019-09-12 11:31:06 +0100

    Changed api's url3

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { Button, Form, Header, Segment } from "semantic-ui-react";
+import {
+  Button,
+  Form,
+  Header,
+  Segment,
+  Grid,
+  GridColumn
+} from "semantic-ui-react";
 
 const LoginForm = ({ submit, routerProps, user }) => {
   const [username, setUsername] = useState("");
@@ -11,107 +18,113 @@ const LoginForm = ({ submit, routerProps, user }) => {
 
   return (
     <div className="inventory-body">
-      <Header
-        style={{
-          fontSize: "6em",
-          textAlign: "left",
-          margin: "0.2em"
-        }}
-      >
-        Catz-Play<i className="fas fa-cat"></i>
-      </Header>
-      <Segment
-        style={{
-          position: "relative",
-          left: "78em",
-          width: "20em",
-          height: "30em",
-          fontSize: "24px",
-          textAlign: "center"
-        }}
-      >
-        <Form
-          error={{
-            content: "Please enter your first name",
-            pointing: "below"
-          }}
-          className="form"
-          size="large"
-          onSubmit={e => {
-            e.preventDefault();
-            submit({ username, password });
-            setUsername("");
-            setPassword("");
-          }}
-        >
+      <Grid columns={2}>
+        <Grid.Column width={12}>
           <Header
             style={{
-              fontSize: "2.5em",
-              textAlign: "center",
-              margin: "1em"
+              fontSize: "6em",
+              textAlign: "left",
+              margin: "0.2em"
             }}
           >
-            Let's play!
+            Catz-Play<i className="fas fa-cat"></i>
           </Header>
-          <Form.Input
+        </Grid.Column>
+        <Grid.Column width={2}>
+          <Segment
             style={{
-              height: "5em",
-              width: "25em"
+              top: "6em",
+              position: "relative",
+              width: "20em",
+              height: "30em",
+              fontSize: "24px",
+              textAlign: "center"
             }}
-            centered
-            // error={{ content: 'Please enter your username', pointing: 'below' }}
-
-            icon="user"
-            iconPosition="left"
-            placeholder="Username"
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-          />
-          <Form.Input
-            style={{
-              height: "5em",
-              width: "25em"
-            }}
-            // error={{ content: 'Please enter your password', pointing: 'below' }}
-            centered
-            icon="lock"
-            iconPosition="left"
-            placeholder="Password"
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-          <Button
-            style={{
-              fontSize: "1.5em",
-              height: "2.7em",
-              width: "9em",
-              margin: "1em"
-            }}
-            color="orange"
-            textAlign="center"
           >
-            Login
-          </Button>
-        </Form>
-        <Header
-          style={{ fontSize: "1.2em", margin: "1.5em" }}
-          textAlign="center"
-        >
-          Not registered yet?
-        </Header>
-        <Button
-          style={{
-            height: "3em",
-            width: "10em",
-            margin: "1em"
-          }}
-          onClick={() => routerProps.history.push("/signup")}
-          color="orange"
-        >
-          Sign Up
-        </Button>
-      </Segment>
+            <Form
+              error={{
+                content: "Please enter your first name",
+                pointing: "below"
+              }}
+              className="form"
+              size="large"
+              onSubmit={e => {
+                e.preventDefault();
+                submit({ username, password });
+                setUsername("");
+                setPassword("");
+              }}
+            >
+              <Header
+                style={{
+                  fontSize: "2.5em",
+                  textAlign: "center",
+                  margin: "1em"
+                }}
+              >
+                Let's play!
+              </Header>
+              <Form.Input
+                style={{
+                  height: "5em",
+                  width: "25em"
+                }}
+                centered
+                // error={{ content: 'Please enter your username', pointing: 'below' }}
+
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+              <Form.Input
+                style={{
+                  height: "5em",
+                  width: "25em"
+                }}
+                // error={{ content: 'Please enter your password', pointing: 'below' }}
+                centered
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+              <Button
+                style={{
+                  fontSize: "1.5em",
+                  height: "2.7em",
+                  width: "9em",
+                  margin: "1em"
+                }}
+                color="orange"
+                textAlign="center"
+              >
+                Login
+              </Button>
+            </Form>
+            <Header
+              style={{ fontSize: "1.2em", margin: "1.5em" }}
+              textAlign="center"
+            >
+              Not registered yet?
+            </Header>
+            <Button
+              style={{
+                height: "3em",
+                width: "10em",
+                margin: "1em"
+              }}
+              onClick={() => routerProps.history.push("/signup")}
+              color="orange"
+            >
+              Sign Up
+            </Button>
+          </Segment>
+        </Grid.Column>
+      </Grid>
     </div>
   );
 };
